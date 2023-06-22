f1a390b31b5fac7daa4886276b9cbe144d1d8d5a
Marcelo Nicolosi <celonicolosi@hotmail.com>
2023-06-22 14:03:44 +0100

    Remove T21/ from the folder path

--- T21/watch_next.py
+++ T21/watch_next.py
@@ -7,7 +7,7 @@ import spacy
 nlp = spacy.load("en_core_web_md")
 
 # Read in the movies.txt file. Each separate line is a description of a different movie.
-with open("T21/movies.txt", "r") as f:
+with open("movies.txt", "r") as f:
     movies = f.readlines()
 
 planet_hulk = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk lands on the planet Sakaar where he is sold into slavery and trained as a gladiator."
